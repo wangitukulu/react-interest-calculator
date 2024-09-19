67ad45e1578a7976843c4f1fd34bf19d0c3f3e29
wonda wasilua <wangitukulu@gmail.com>
2024-09-19 23:27:42 +0200

    search changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import './App.css';
-
+// react 
 function App() {
   const[deposit, setDeposit] = useState("");
   const[final, setFinal] = useState(0);
