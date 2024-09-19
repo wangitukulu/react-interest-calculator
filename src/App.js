dcfa5836334643a6d60e1a1ddd4e744adf76fea4
wonda wasilua <wangitukulu@gmail.com>
2024-09-19 23:15:07 +0200

    second changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from "react";
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const[deposit, setDeposit] = useState("");
+  const[final, setFinal] = useState(0);
+
+const AddDeposit= ()=>{
+  const amountDeposit = parseInt(deposit);
+  if(amountDeposit >0 && !isNaN(amountDeposit)){
+     setDeposit((preFinal)=> preFinal + amountDeposit); 
+     setDeposit("");
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <label for='deposit' >deposit money: </label>
+      <input type="number" value={deposit} onChange={(event) => setDeposit(event.target.value)}  /> {/* An input for the user to deposit money to the
+bank   */}
+      <button onClick={AddDeposit}>click here</button> {/* click a button that will
+add the “deposit” amount to the currently displayed bank balance). */}
+      <h3> deposit money : {deposit}</h3> 
+      <h1> After adding : {final}</h1>
+
+    
     </div>
-  );
+    
+  )
+ 
 }
 
 export default App;
+
---
+++ src/Charge.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+
+function ChargeBank(props) {
+    const [charge, setCharge] = useState("");
+    const chargeRate = final - final / 10
+    return (
+        <div>
+            {/* charge rate of the bank*/}
+            <h1>Charge bank: {chargeRate}</h1>
+            <label for="charge bank">charge bank</label>
+            <input type="number" placeholder="Enter 10% charge bank" />
+            <button onclick="ChargeBank()">Add Interest</button>
+        </div>
+    );
+
+
+}
+
+export default ChargeBank;
+
